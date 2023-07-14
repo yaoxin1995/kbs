412b0bc7568c064da72466e62980dc4f5f63bfa8
yaoxin <yaoxin19950517@gmail.com>
2023-07-14 11:50:26 +0200

    update policy

--- src/api_server/src/attest/native.rs
+++ src/api_server/src/attest/native.rs
@@ -112,27 +112,20 @@ pub struct AttestationReport {
 	pub signature: SnpAttestationReportSignature  /* 0x2A0 */
 }
 
-#[derive(Serialize, Deserialize, Debug, Default, Clone)]
-pub struct SingleShotCommandLineModeConfig {
-    pub allowed_cmd: Vec<String>,
-    pub allowed_dir: Vec<String>,
-}
 
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub struct PrivilegedUserConfig {
-    pub enable_terminal: bool,
-    pub enable_single_shot_command_line_mode: bool,
-    pub single_shot_command_line_mode_configs : SingleShotCommandLineModeConfig,
-    pub exec_result_encryption: bool,
-    pub enable_container_logs_encryption:bool,
+    pub allowed_cmd: Vec<String>,
+    pub allowed_dir: Vec<String>,
+    pub no_interactive_process_stdout_err_encryption:bool,
+    pub interactive_porcess_stdout_err_encryption:bool,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub struct UnprivilegedUserConfig {
-    pub enable_terminal: bool,
-    pub enable_single_shot_command_line_mode: bool,
-    pub single_shot_command_line_mode_configs : SingleShotCommandLineModeConfig,
+    pub allowed_cmd: Vec<String>,
+    pub allowed_dir: Vec<String>,
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug, Default)]
